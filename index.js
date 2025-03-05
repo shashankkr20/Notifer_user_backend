f3594d46af42510a6aabe8590975dd5857cdac5f
pandiji <skm808478@gmail.com>
2025-03-05 13:16:25 +0530

    updated index.js

--- index.js
+++ index.js
@@ -11,13 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-    cors({
-      origin: "exp://192.168.1.8:8081",
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-      credentials: true, // If using cookies or authentication
-    })
-  );
+app.use(cors()
+)
 app.use(express.json());
 
 // Routes
